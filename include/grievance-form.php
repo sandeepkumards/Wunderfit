a9b214ce09dd404e2acf595eadf03f045e6d8b5c
family <family@gorenteds-MacBook-Pro.local>
2019-01-15 17:49:34 +0530

    changes in grievance form

--- include/grievance-form.php
+++ include/grievance-form.php
@@ -10,11 +10,11 @@ parse_str($_POST['data'], $post_data);
 
 
 		$user_name = stripslashes(strip_tags(trim($post_data['username'])));
-		$user_accountno = stripslashes(strip_tags(trim($post_data['account no'])));
+		$user_accountno = stripslashes(strip_tags(trim($post_data['accountNo'])));
 		$user_grievance =stripslashes(strip_tags(trim($post_data['grievance'])));
 		if (trim($contact_email)!='') {
-			$subj = 'Enquiry form via wunderfit website';
-			$msg = $subj." \r\nName: $user_name  \r\nAccount No: $account_no \r\nGrievance: $user_grievance";
+			$subj = 'Grievance form via wunderfit website';
+			$msg = $subj." \r\nName: $user_name  \r\nAccount No: $user_accountno \r\nGrievance: $user_grievance";
 
 			$head = "Content-Type: text/plain; charset=\"utf-8\"\n"
 				. "X-Mailer: PHP/" . phpversion() . "\n"
